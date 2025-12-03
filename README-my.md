67787bd13b571017e5bfd5d09a4bb1ae7a9add8c
wpl <843145936@qq.com>
2025-12-04 00:10:45 +0800

    修改 png & docker-image-dev.yml

---
+++ README-my.md
@@ -0,0 +1,15 @@
+开发环境:
+本地存储 不支持/admin 修改
+
+
+配置
+Upstash
+
+
+USERNAME=admin
+PASSWORD=123456
+NEXT_PUBLIC_STORAGE_TYPE=upstash
+UPSTASH_URL=https://guided-feline-36452.upstash.io
+UPSTASH_TOKEN=AY5kAAIncDE0ZjJjZGE0YjhiZmU0NzYwODk4ZDdiMmZkMDNjODJhZHAxMzY0NTI
+
+https://guided-feline-36452.upstash.io
--- scripts/generate-manifest.js
+++ scripts/generate-manifest.js
@@ -26,22 +26,22 @@ const manifestTemplate = {
   'apple-mobile-web-app-status-bar-style': 'black',
   icons: [
     {
-      src: '/icons/icon-192x192.png',
+      src: '/icons/icon-192.png',
       sizes: '192x192',
       type: 'image/png',
     },
+    // {
+    //   src: '/icons/icon-256x256.png',
+    //   sizes: '256x256',
+    //   type: 'image/png',
+    // },
+    // {
+    //   src: '/icons/icon-384x384.png',
+    //   sizes: '384x384',
+    //   type: 'image/png',
+    // },
     {
-      src: '/icons/icon-256x256.png',
-      sizes: '256x256',
-      type: 'image/png',
-    },
-    {
-      src: '/icons/icon-384x384.png',
-      sizes: '384x384',
-      type: 'image/png',
-    },
-    {
-      src: '/icons/icon-512x512.png',
+      src: '/icons/icon-512.png',
       sizes: '512x512',
       type: 'image/png',
     },
